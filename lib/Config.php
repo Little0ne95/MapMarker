e0f1c5aaf9e40054bd813cffd10bb37d7287c019
Rachel Burke <rachel.burke@cvknowhow.com>
2019-09-12 16:30:43 +0100

    removing database stuff

--- lib/Config.php
+++ lib/Config.php
@@ -1,16 +1,6 @@
 <?php
 session_start();
 
-Config::write('production', '0'); //live / test database settings + hides
-Config::write('show_debug', '1'); //hides config messages etc
-
-// MYSQL DATABASE
-Config::write('mysql.db.host', '127.0.0.1');
-Config::write('mysql.db.basename', 'carpool');
-Config::write('mysql.db.user', 'root');
-Config::write('mysql.db.password', '');
-
-
 Config::write('office.location', '');
 Config::write('api.key', 'API_KEY');
 
